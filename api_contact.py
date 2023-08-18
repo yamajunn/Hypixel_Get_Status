94215cb463480b363f5bcfffc0531273af434f24
yamajunn <amiton1115@gmail.com>
2023-08-19 01:07:40 +0900

    add bot

---
+++ api_contact.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from pprint import pprint
+
+def bedwars_status(name):
+    import requests
+    import json
+    from pprint import pprint
+
+    if type(name) == str:
+        
+        def getuuid(call):
+            r = requests.get(call,timeout=10)
+            return r.json()
+
+        def getinfo(call):
+            r = requests.get(call,timeout=10)
+            return r.json()
+
+        name_link = f"https://api.mojang.com/users/profiles/minecraft/{name}"
+
+        uuid = getuuid(name_link)["id"]
+
+        API_KEY = "a6d34c13-e3f8-423d-b3b0-a59a02ab3d2b"
+
+        uuid_link = f"https://api.hypixel.net/player?key={API_KEY}&uuid={uuid}"
+
+
+        # pprint(getinfo(uuid_link))
+
+        if getinfo(uuid_link)["success"] == True:
+            data = ""
+            dic_list = ['wins_bedwars', 'losses_bedwars', 'final_kills_bedwars', 'final_deaths_bedwars', 'kills_bedwars', 'deaths_bedwars', 'beds_broken_bedwars' , 'beds_lost_bedwars']
+            data += "star:" + str(getinfo(uuid_link)["player"]['achievements']['bedwars_level']) + "\n"
+            for item in dic_list:
+                data += f"{item}:" + str(getinfo(uuid_link)["player"]["stats"]["Bedwars"][item]) + "\n"
+        return data
+    else:
+        return "name error"
---
+++ status_bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord import app_commands
+from api_contact import bedwars_status
+
+TOKEN = "MTE0MTc0NzExNTAwNjA5OTQ4Ng.G9XWqS.4tUkXqR4oOvpkwK9Re0JCu0sy3Ljsgu3UDcoGE"
+
+intents = discord.Intents.default()#適当に。
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    print("Logged in as hypixel status")
+    await tree.sync()#スラッシュコマンドを同期
+    print("command sync")
+
+@tree.command(name="command",description="コマンドリストを表示")
+async def test_command(interaction: discord.Interaction):
+    await interaction.response.send_message("/command : コマンドリストを表示する\n/oumu [str] : strをオウム返し")
+
+@tree.command(name="oumu",description="オウム返し")
+async def test_command(interaction: discord.Interaction,text:str):
+    await interaction.response.send_message(text)
+
+@tree.command(name="bedwars",description="ベッドウォーズのステータス")
+async def test_command(interaction: discord.Interaction,name:str):
+    data = bedwars_status(name)
+    await interaction.response.send_message(data)
+
+
+client.run(TOKEN)
