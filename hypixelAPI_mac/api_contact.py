38e412a772f234eb5689fb12f8eeb7d9f479723f
Junsei Yamazaki <117433834+yamajunn@users.noreply.github.com>
2024-06-26 18:37:22 +0900

    Update api_contact.py

--- hypixelAPI_mac/api_contact.py
+++ hypixelAPI_mac/api_contact.py
@@ -13,7 +13,7 @@ def bedwars_status(name):
 
         name_link = f"https://api.mojang.com/users/profiles/minecraft/{name}"
         uuid = getuuid(name_link)["id"]
-        API_KEY = "d8a6ad35-da45-440f-9218-c3030fa65ced"
+        API_KEY = ""
         uuid_link = f"https://api.hypixel.net/player?key={API_KEY}&uuid={uuid}"
 
         data_dic = getinfo(uuid_link)
